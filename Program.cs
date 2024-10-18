814bf75733336cc10edd5e60956f17db964fa4dd
vecseizsolt <vecseizs@kkszki.hu>
2024-10-18 11:33:06 +0200

    A második feladat nagyjából elkészült, használatra kész.

--- Program.cs
+++ Program.cs
@@ -8,22 +8,30 @@ namespace Dolgozat_10_18_02_feladat
 {
     internal class Program
     {
-        static void Main(string[] args)
+        static void jelszo()
         {
             // 2.feladat
 
             string jelszo = "";
+            string[] szamok = new string["1", "2", "3", "4", "5", "6", "7", "8", "9", "0", ];
 
             Console.WriteLine("Kérem a jelszót: ");
             jelszo = Console.ReadLine();
 
-            while (jelszo.Length < 8) 
+            while (jelszo.Length < 8)
             {
                 Console.WriteLine("A jelszó nem felel meg a kritériumoknak, próbálja újra: ");
                 jelszo = Console.ReadLine();
             }
 
             Console.WriteLine("Jelszó elfogadva!");
+
+            Console.ReadKey();
+        }
+
+        static void Main(string[] args)
+        {
+            jelszo();
         }
     }
 }
